22b960f2e90c7f81e112049813fd1662291e78ad
Sai Chandra Mouli Tadiparthi <cb.en.u4cse17160@cb.students.amrita.edu>
2020-03-15 16:13:05 +0530

    Updates

---
+++ precache-manifest.30940f0c4ba2ba76f92a7c603b6780a2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c96fb91f71831f5f09c77c281f89fd0",
+    "url": "/SE_frontend/index.html"
+  },
+  {
+    "revision": "f682f13a4a93d79b9225",
+    "url": "/SE_frontend/static/css/2.e12f28a5.chunk.css"
+  },
+  {
+    "revision": "fc04258d2e977bda670e",
+    "url": "/SE_frontend/static/css/main.2241ef4f.chunk.css"
+  },
+  {
+    "revision": "f682f13a4a93d79b9225",
+    "url": "/SE_frontend/static/js/2.92d59c4c.chunk.js"
+  },
+  {
+    "revision": "66bd9d5f4d5afeb1dd021ae67b639598",
+    "url": "/SE_frontend/static/js/2.92d59c4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc04258d2e977bda670e",
+    "url": "/SE_frontend/static/js/main.0729e0d0.chunk.js"
+  },
+  {
+    "revision": "e101f2c34a827ce08117",
+    "url": "/SE_frontend/static/js/runtime-main.362190b4.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/SE_frontend/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "305c2ac7234efb6806722d5d8a980e00",
+    "url": "/SE_frontend/static/media/background.305c2ac7.jpg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/SE_frontend/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/SE_frontend/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/SE_frontend/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/SE_frontend/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "d546ecc2cab41e459ef6859e4131a08b",
+    "url": "/SE_frontend/static/media/sekalu.d546ecc2.jpg"
+  }
+]);
